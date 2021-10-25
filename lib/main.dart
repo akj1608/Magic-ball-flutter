8cfedc7ee5af5ef65fe992a28b5bd37e51c18d0c
akj1608 <akj1608@gamil.com>
2021-10-25 21:42:50 +0530

    files added from desktop to remote repo

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class  BallPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    backgroundColor: Colors.blue,
+    appBar: AppBar(
+backgroundColor: Colors.blue.shade900,
+title: Center(
+child: Text('8 balls'
+),
+),
+),
+body: Ball(),
+);
+  }
+}
+
+
+class Ball extends StatefulWidget {
+    @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var choose =1;
+
+  void update(){
+    setState(() {
+      choose=Random().nextInt(5)+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return (
+    Center(
+      child: Expanded(
+      child:FlatButton(
+        onPressed: (){
+          update();
+        },
+          child: Image.asset('images/ball$choose.png')
+      ),
+      )
+    )
+    );
+  }
+}
+
